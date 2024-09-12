53776eecafae69fb6b008d947ed77a4981073a09
Fernando Fantinel <fantinel89@gmail.com>
2024-09-12 19:11:51 -0300

    adiciona menu

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-const start = () => {
-  while (true) {
+const { select } = require('@inquirer/prompts')
 
+const start = async () => {
+  while (true) {
+    const opcao = await select({
+      message: "Menu >",
+      choices: [
+        {
+          name: "Cadastrar meta",
+          value: "cadastrar"
+        },
+        {
+          name: "Listar metas",
+          value: "listar"
+        },
+        {
+          name: "Sair",
+          value: "sair"
+        }
+      ]
+    })
+
+    switch (opcao) {
+      case "cadastrar":
+        console.log("cadastrar")
+        break;
+      case "listar":
+        console.log("listar")
+        break;
+      case "sair":
+        console.log("Até mais!")
+        return
+    }
   }
 }
 
