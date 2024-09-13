b59f3d64a6e95b0f6944845598183532c9839b20
Fernando Fantinel <fantinel89@gmail.com>
2024-09-13 15:01:11 -0300

    resolve problema metas marcadas/desmarcadas

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ const listarMetas = async () => {
     choices: [...metas],
   })
 
+  metas.forEach(meta => {
+    meta.checked = false
+  })
+
   if (respostas.length === 0) {
     console.log("Nenhuma meta selecionada.")
     return
   }
-
-  metas.forEach(meta => {
-    meta.checked = false
-  })
 
   respostas.forEach(resposta => {
     const meta = metas.find(m => {
